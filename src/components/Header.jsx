179b31350b991108092580d4cbfeeb81ecc55ac3
Wasim <wasimbhai6205@gmail.com>
2024-03-24 15:20:06 +0530

    create header section

---
+++ src/components/Header.jsx
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import { RxCaretDown } from "react-icons/rx";
+import { IoSearchSharp } from "react-icons/io5";
+import { CiDiscount1 } from "react-icons/ci";
+import { IoHelpBuoyOutline } from "react-icons/io5";
+import { FaRegUser } from "react-icons/fa6";
+import { PiShoppingCart } from "react-icons/pi";
+
+const Header = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const showSideMenu = () => {
+    console.log("hi");
+    setToggle(true);
+  };
+
+  const hideSideMenu = () => {
+    setToggle(false);
+  };
+
+  const links = [
+    {
+        icon: <IoSearchSharp />,
+        name: "Search",
+    },
+    {
+        icon: <CiDiscount1 />,
+        name: "Offers",
+        sup: "New"
+    },
+    {
+        icon: <IoHelpBuoyOutline />,
+        name: "Help",
+    },
+    {
+        icon: <FaRegUser />,
+        name: "Sign In",
+    },
+    {
+        icon: <PiShoppingCart />,
+        name: "Cart",
+    },
+  ]
+
+  return (
+    <>
+      <div
+        className="black-overlay w-full h-full fixed duration-500"
+        onClick={hideSideMenu}
+        style={{
+          opacity: toggle ? 1 : 0,
+          visibility: toggle ? "visible" : "hidden",
+        }}
+      >
+        <div
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
+          className="w-[500px] bg-white h-full absolute duration-[400ms]"
+          style={{
+            left: toggle ? "0%" : "-100%",
+          }}
+        ></div>
+      </div>
+      <header className="p-[15px] shadow-xl text-[#686b78]">
+        <div className="max-w-[1200px] mx-auto flex items-center">
+          <div className="w-[100px]">
+            <img src="images/logo.png" className="w-full" alt="logo" />
+          </div>
+          <div className="">
+            <span className="font-bold border-b-[3px] border-[black]">
+              Other
+            </span>{" "}
+            Ranchi, Jharkhand, India{" "}
+            <RxCaretDown
+              onClick={showSideMenu}
+              fontSize={25}
+              className="inline cursor-pointer text-[#fc8019]"
+            />
+          </div>
+            <nav className="flex list-none gap-7 ml-auto font-semibold text-[18px]">
+                {
+                    links.map((link, index) => (
+                        <li key={index} className="flex cursor-pointer hover:text-[#fc8019] items-center gap-2">
+                            {link.icon}
+                            {link.name}
+                            <sup className="text-[#fc8019]">{link.sup}</sup>
+                        </li>
+                    ))
+                }
+            </nav>
+          </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
