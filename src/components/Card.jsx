d2a7d12078eea1a0df4b426238e09698aa6f4f63
Wasim <wasimbhai6205@gmail.com>
2024-03-27 05:28:49 +0530

    slider created

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,17 +2,28 @@ import React from "react";
 
 const Card = (props) => {
   return (
-    <div className={`${props.width} shrink-0 cursor-pointer mb-3`}>
+    <div
+      style={{
+        transform: `translateX(-${props.slide * 100}%)`,
+      }}
+      className={`${props.width} shrink-0 cursor-pointer mb-3 duration-500`}
+    >
       <div className="group h-[182px] rounded-[15px] overflow-hidden relative">
-        <img className="group-hover:scale-110 duration-150 object-cover w-full h-full" src={props.img} alt="" />
+        <img
+          className="group-hover:scale-110 duration-150 object-cover w-full h-full"
+          src={props.img}
+          alt=""
+        />
         <div className="image-overlay absolute w-full h-full top-0 flex items-end p-2 text-[25px] font-bold text-white tracking-tighter">
           {props.offer}
         </div>
       </div>
-      <div className="mt-3 text-xl font-bold">{props.title}</div>
-      <div >
+      <div className="mt-3 md:text-xl font-bold">{props.title}</div>
+      <div>
         <Star className="inline" /> {props.rating}
-        <span className="ml-2">{props.minTime} - {props.maxTime} mins</span>
+        <span className="ml-2">
+          {props.minTime} - {props.maxTime} mins
+        </span>
       </div>
       <div className="text-slate-700">
         {props.name}
@@ -28,7 +39,7 @@ export default Card;
 const Star = (props) => {
   return (
     <svg
-    className={props.className}
+      className={props.className}
       width={20}
       height={20}
       viewBox="0 0 20 20"
--- src/components/TopRest.jsx
+++ src/components/TopRest.jsx
@@ -6,6 +6,17 @@ import { RestData } from "../RestData";
 
 const TopRest = () => {
   const [data, setData] = useState(RestData);
+  const [slide, setSlide] = useState(0);
+
+  const nextSlide = () => {
+    if(data.length - 4 == slide) return false;
+    setSlide(slide + 2)
+  }
+
+  const prevSlide = () => {
+    if(slide == 0) return false;
+    setSlide(slide - 2)
+  }
 
   return (
     <div className="max-w-[1200px] mx-auto px-2 mb-[100px]">
@@ -14,10 +25,10 @@ const TopRest = () => {
           Top restaurant chains in Ranchi
         </div>
         <div className="flex">
-          <div className="cursor-pointer flex justify-center items-center w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2">
+          <div onClick={prevSlide} className="cursor-pointer flex justify-center items-center w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2">
             <FaArrowLeft />
           </div>
-          <div className="cursor-pointer flex justify-center items-center w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2">
+          <div onClick={nextSlide} className="cursor-pointer flex justify-center items-center w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2">
             <FaArrowRight />
           </div>
         </div>
@@ -26,7 +37,7 @@ const TopRest = () => {
       <div className="flex gap-5 overflow-hidden">
         {
             data.map((d,i) => (
-                <Card {...d} key={i} width="w-full md:w-[273px]" />
+                <Card slide={slide} {...d} key={i} width="w-full md:w-[273px]" />
             ))
         }
       </div>
